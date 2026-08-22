9ccf997d09a9481228d582bf1a75e34c3bfb1145
Siddharth Karthikeyan <255224360+spiderwool@users.noreply.github.com>
2026-08-22 14:39:31 -0400

    Finished watermelon and nearly completed team

--- learning_cpp/A_team/A_team.cpp
+++ learning_cpp/A_team/A_team.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
+int main(){
     int number_of_problems;
-    int agreed_problems;
+    int agreed_problems = 0;
     int first, second, third;
     std::cin >> number_of_problems;
+    while(number_of_problems > 0){
     std:: cin >> first >> second >> third;
-    std:: cout << first << " " << second << " " << third << endl;
+    if((first + second + third) > 1){
+        agreed_problems++;
+    }
+    number_of_problems--;
+    }
+
+    std:: cout << agreed_problems << "\n";
     
 }
