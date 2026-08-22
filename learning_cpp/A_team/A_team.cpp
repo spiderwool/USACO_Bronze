9257d90355b37d34b176c0cfe41a1963dcee464f
Siddharth Karthikeyan <255224360+spiderwool@users.noreply.github.com>
2026-08-22 11:52:47 -0400

    Did watermelon.cpp and started A-team

---
+++ learning_cpp/A_team/A_team.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int number_of_problems;
+    int agreed_problems;
+    int first, second, third;
+    std::cin >> number_of_problems;
+    std:: cin >> first >> second >> third;
+    std:: cout << first << " " << second << " " << third << endl;
+    
+}
